be24d40a59cc51da99696d1770b674503363693e
sreevani <sreevani.r@ibm.com>
2021-06-04 11:00:47 +0530

    intial commit

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const express=require('express');
-
-var oracledb= require('oracledb');
-
-const app = express();
-
-app.get('/', (req,res) => {
- res.send('hello world');
+const { Client } = require('pg');
+const client = new Client({
+    user: 'squad1',
+    host: '3.143.104.1',
+    database: 'bootcamp',
+    password: 'Squ@d456',
+    port: 5432,
 });
-
-app.get('/api/courses', (req,res) => {
-   res.send([1,2,3]);
-});
-
-var connection= oracledb.connection(
-  {
-    host : 'localhost' ,
-    user :  'SYSTEM' ,
-    password : 'tiger' ,
-    database : 'con1'
-  }
-);
-
-connection.connect(function(err){
+client.connect(function(err){
   if (err) throw err;
   console.log('connected..');
 });
 
+const db = require('./queries')
+
+const app=express()
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+ app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
+
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
 app.listen(3000, () => console.log('listening on port 3000...'));
